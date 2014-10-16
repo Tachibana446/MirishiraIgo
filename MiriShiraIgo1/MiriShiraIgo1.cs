e1871a07128f77be491daa4f8c8738849de78adb
Tachibana <stinger33z@gmail.com>
2014-10-16 15:45:49 +0900

    石を置く処理をPutStone()メソッドに分割

--- MiriShiraIgo1/MiriShiraIgo1.cs
+++ MiriShiraIgo1/MiriShiraIgo1.cs
@@ -10,7 +10,11 @@ namespace MiriShiraIgo1
     class MiriShiraIgo1
     {
         static int boardSize = 480;
-        static int cellSize = boardSize / 16;
+        static int cellSize = boardSize / 16;        
+        // クリックの履歴
+        static bool click = false;
+        // 石をおいた場所の記録
+        static List<Tuple<int,int>> stones = new List<Tuple<int, int>>();
 
         static void Main(String[] args)
         {
@@ -23,12 +27,6 @@ namespace MiriShiraIgo1
             {
                 throw new Exception("DxLibの初期化に失敗");
             }
-
-            // このへんに変数。あとでクラスに分けること
-            // クリックの履歴
-            var click = false;
-            // 石をおいた場所の記録
-            var stones = new List<Tuple<int, int>>();
 
             // 盤面の表示
             DrawBoard();
@@ -43,30 +41,7 @@ namespace MiriShiraIgo1
                 // 主処理
                 
                 // 石を置く
-
-                if ((DX.GetMouseInput() & DX.MOUSE_INPUT_LEFT) != 0 && click == false)
-                {
-                    click = true;
-                    int x, y;
-                    DX.GetMousePoint(out x, out y);
-                    // そいつがクリックしたかった本来の場所を推測
-                    int tx = (x + cellSize/2) / cellSize;
-                    int ty = (y + cellSize/2) / cellSize;
-                    // そこに石を置いたことにする
-                    stones.Add( new Tuple<int, int>(tx, ty) );
-                    
-                    // DEBUG
-                    foreach (var item in stones)
-                    {
-                        System.Diagnostics.Debug.WriteLine(item.Item1.ToString() + "," + item.Item2.ToString());
-                    }
-
-                }
-                if ((DX.GetMouseInput() & DX.MOUSE_INPUT_LEFT) == 0 && click)
-                {
-                    System.Diagnostics.Debug.WriteLine("0");
-                    click = false;
-                }
+                PutStones();
 
                 // 石の描画
                 bool turn = true;
@@ -108,6 +83,35 @@ namespace MiriShiraIgo1
                     DX.DrawCircle(cellSize * i, cellSize * j, 2, DX.GetColor(0, 0, 0), DX.TRUE);
                 }
             }
+        }
+
+        // 石を置く処理
+        static void PutStones()
+        {
+            if ((DX.GetMouseInput() & DX.MOUSE_INPUT_LEFT) != 0 && click == false)
+            {
+                click = true;
+                int x, y;
+                DX.GetMousePoint(out x, out y);
+                // そいつがクリックしたかった本来の場所を推測
+                int tx = (x + cellSize / 2) / cellSize;
+                int ty = (y + cellSize / 2) / cellSize;
+                // そこに石を置いたことにする
+                stones.Add(new Tuple<int, int>(tx, ty));
+
+                // DEBUG
+                foreach (var item in stones)
+                {
+                    System.Diagnostics.Debug.WriteLine(item.Item1.ToString() + "," + item.Item2.ToString());
+                }
+
+            }
+            if ((DX.GetMouseInput() & DX.MOUSE_INPUT_LEFT) == 0 && click)
+            {
+                System.Diagnostics.Debug.WriteLine("0");
+                click = false;
+            }
+
         }
     }
 }
